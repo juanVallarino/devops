a2927c90ad1930d30487a91f91b4e4e78e2844e0
Juan Vallarino <vallarinojuansebastian@gmail.com>
2025-10-20 15:25:02 -0500

    ajuste ambientesss

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 import os
-from app import create_app
+from app import create_app, db
 
 app = create_app(os.getenv('FLASK_ENV', 'development'))
 
-if __name__ == '__main__':
+def init_db():
+    with app.app_context():
+        db.create_all()
+        print("Base de datos inicializada correctamente")
+        print("Tablas creadas:")
+        print("- blacklists")
+
+if __name__ == '_main_':
+    init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
