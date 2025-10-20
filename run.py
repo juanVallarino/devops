0fef1bb92455fb7cda5c7376da1bae27c916b561
Juan Vallarino <vallarinojuansebastian@gmail.com>
2025-10-20 16:16:43 -0500

    ajuste ambientes ordenada

--- run.py
+++ run.py
@@ -3,13 +3,10 @@ from app import create_app, db
 
 app = create_app(os.getenv('FLASK_ENV', 'development'))
 
-def init_db():
+if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("Base de datos inicializada correctamente")
         print("Tablas creadas:")
         print("- blacklists")
-
-if __name__ == '_main_':
-    init_db()
     app.run(debug=True, host='0.0.0.0', port=5000)
